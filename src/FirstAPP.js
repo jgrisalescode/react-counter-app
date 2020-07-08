7b95fca98e315d453b9f2e400f853291c58aba9f
JuliaGrisales <jgrisales@gmail.com>
2020-07-08 21:59:48 +0200

    Print variables on HTML

--- src/FirstAPP.js
+++ src/FirstAPP.js
@@ -2,11 +2,22 @@
 import React from "react"
 
 const FirstApp = () => {
-  // Use Fragment or <></> or <div></div> for return several lines of HTML
+  const congrats = "Hello World!!"
+  const numbers = 1254.548789
+  const boolean = true
+  const array = [1, 2, 3, 4, 5]
+  const anObject = {
+    name: "Julian",
+    age: 40
+  }
   return (
     <>
-      <h1>Hello World!!</h1>
-      <p>This is a paragraph</p>
+      <h1>{congrats}</h1>
+      <p>{numbers}</p>
+      <p>{boolean}</p>
+      <p>{array}</p>
+      <p>{JSON.stringify(anObject)}</p>
+      <pre>{JSON.stringify(anObject)}</pre>
     </>
   )
 }
