c9ef798022e1139a9417949e02c3b042bcb32289
JuliaGrisales <jgrisales@gmail.com>
2020-07-09 12:09:17 +0200

    CounterApp component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import FirstAPP from "./FirstAPP"
+// import FirstAPP from "./FirstAPP"
+import CounterApp from "./CounterApp"
 import "./index.css"
 
 const divRoot = document.querySelector("#root")
-ReactDOM.render(<FirstAPP congratsSayayin="Hi, I'm Kakaroto" />, divRoot)
+// ReactDOM.render(<FirstAPP congratsSayayin="Hi, I'm Kakaroto" />, divRoot)
+ReactDOM.render(<CounterApp value={100} />, divRoot)
